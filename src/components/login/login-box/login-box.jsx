2c8f75ab876283c495e12d67935441bcb3605f89
Andrey Simonov <sasmeo@gmail.com>
2023-01-07 04:22:16 +0600

    Фикс1

--- src/components/login/login-box/login-box.jsx
+++ src/components/login/login-box/login-box.jsx
@@ -5,7 +5,7 @@ import { NavLink } from 'react-router-dom';
 const LoginBox = () => {
     return (
         <div className={styles.login}>
-            <NavLink to="/profile" exact={true} className={styles.login_link} activeClassName={styles.active}>
+            <NavLink to="/profile"  className={styles.login_link} activeClassName={styles.active}>
                 <ProfileIcon type="secondary"/>
                 <p className="text text_type_main-default ml-2">
                     Личный кабинет
